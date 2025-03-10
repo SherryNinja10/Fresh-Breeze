108a9889af421a294e5b7feaf243bf44d6a36a3d
SherryNinja10 <aarshdeepsinghvandal@gmail.com>
2025-03-10 00:47:17 -0700

    debugging part 2

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,13 +5,24 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DATABASE,
     password: process.env.PASSWORD,
     port: process.env.PORT,
     connectionLimit: 10,
+    reconnect: true,
     waitForConnections: true,
     queueLimit: 0
 });
+
+pool.on('error', (err) => {
+    console.error('MySQL Pool Error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Attempting to reconnect...');
+        pool.connect();
+    }
+});
+
+export { pool };
